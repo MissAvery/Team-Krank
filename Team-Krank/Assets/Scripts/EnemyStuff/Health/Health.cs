6b33302ce57d00e3e084dce69cdfdc2264cfa772
lalasagna <l.siekierski@yahoo.com>
2023-02-03 23:39:18 +0100

    Gegner Prototypen
    Chonk mucho HP und klein speedy gegner sind da. die anderen sind basically mit den health und walk und damage skripten die ich gemacht habe, innerhalb von 5 min zu machen abgesehen von den animationen.
    
    Health Skript gubt es und hab auch ein provisorisches fürs damage taken geschrieben und versucht es mit nem trigger zu testen, aber das funktioniert bisher noch nicht so ganz - werd mich nochmal dran setzen

---
+++ Team-Krank/Assets/Scripts/EnemyStuff/Health/Health.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float startHealth;
+    private float currentHealth;
+    private bool dead = false;
+    public GameObject enemy;
+
+    private void Awake(){
+        currentHealth = startHealth;
+    }
+
+    public void damageTaken(float _damage){
+        
+        if (currentHealth > 0){
+            currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startHealth);
+            /*player aua :( */
+        }
+        else {
+            if (dead == false){
+                
+                /*if (GetComponent<Walk>() != null){*/
+                enemy.SetActive(false);
+                dead = true;
+            }
+
+            /* ded :(*/
+        }
+    }
+
+}
