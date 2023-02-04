290e643c1e67116394c1b467e13cae7d1ac870fa
MissAvery <avloenker@gmail.com>
2023-02-04 22:54:52 +0100

    TrapPoints Merged to Main (Not Working)

--- Team-Krank/Assets/Scripts/AudioManager.cs
+++ Team-Krank/Assets/Scripts/AudioManager.cs
@@ -33,6 +33,12 @@ public class AudioManager : MonoBehaviour
         s.source.Play();
     }
 
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Stop();
+    }
+
     public void NewSoundVolume(System.Single vol)
      {
         foreach (Sound s in sounds)
